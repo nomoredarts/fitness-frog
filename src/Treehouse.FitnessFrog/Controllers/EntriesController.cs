5ff361e8aff770a74d6d7c3a063d7536c564f577
paranoid <paranoid@DESKTOP-PC9USH7>
2016-10-16 10:54:46 +0200

    Updated the EntriesController Add method parameter data types

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -46,7 +46,8 @@ namespace Treehouse.FitnessFrog.Controllers
 
         [ActionName("Add")]
         [HttpPost]
-        public ActionResult Add(string date, string activityId, string duration, string intensity, string exclude, string notes)
+        public ActionResult Add(DateTime? date, int? activityId, double? duration, 
+            Entry.IntensityLevel? intensity, bool? exclude, string notes)
         {
             ViewBag.Date = date;
             ViewBag.ActivityId = activityId;
